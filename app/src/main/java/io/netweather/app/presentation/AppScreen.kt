f33ea27edd74e775241ecb2a7927b2b0eee2ad07
OvodokSP <ovodok.sp@yandex.ru>
2026-09-22 01:35:01 +0300

    [no-deploy] Fix Compose state import

--- app/src/main/java/io/netweather/app/presentation/AppScreen.kt
+++ app/src/main/java/io/netweather/app/presentation/AppScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
